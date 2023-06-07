84670749b48cad61487b231d55abea333a0a2285
Adam Żukowski <adam.zukowski471@gmail.com>
2023-06-07 18:43:05 +0200

    Fix styles for lists of benefits

--- src/components/common/FeatureBox/FeatureBox.js
+++ src/components/common/FeatureBox/FeatureBox.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './FeatureBox.module.scss';
 
 const FeatureBox = ({ active, icon, children }) => (
-  <div className={styles.root + (active ? ' ' + styles.active : '')}>
+  <div className={styles.root}>
     {icon && (
       <div className={styles.iconWrapper}>
         <FontAwesomeIcon className={styles.icon} icon={icon} />
