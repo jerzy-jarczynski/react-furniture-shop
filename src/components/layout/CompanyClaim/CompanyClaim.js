6c4911f905c8f28937ce67cbed37a97c9822622d
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-06-07 18:46:33 +0200

    Change attribute in cart counter input

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -26,7 +26,7 @@ const CompanyClaim = () => (
             <div className={styles.cartIcon}>
               <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
             </div>
-            <input className={styles.cartCounter} min={0} max={99999} value={99999} />
+            <input className={styles.cartCounter} maxLength={5} value={99999} />
           </a>
         </div>
       </div>
