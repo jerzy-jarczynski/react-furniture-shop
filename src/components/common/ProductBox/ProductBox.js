a9ad3bd50995aebbff10b722dee880951d8f178b
Dominika Janisz <mikaaa366@gmail.com>
2023-06-12 23:14:10 +0200

    fix favorite

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import styles from './ProductBox.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -10,55 +9,75 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
+import { toggleFavorite } from '../../../redux/productsRedux';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-const ProductBox = ({ name, price, promo, stars }) => (
-  <div className={styles.root}>
-    <div className={styles.photo}>
-      <img
-        className={styles.image}
-        alt={name}
-        src={`${process.env.PUBLIC_URL}/images/products/${name}.jpeg`}
-      />
-      {promo && <div className={styles.sale}>{promo}</div>}
-      <div className={styles.buttons}>
-        <Button variant='small'>Quick View</Button>
-        <Button variant='small'>
-          <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
-        </Button>
+const ProductBox = ({ name, price, promo, id, stars, isFavorite }) => {
+  const [favoriteValue, setFavoriteValue] = useState(isFavorite);
+  const productId = id;
+
+  const dispatch = useDispatch();
+  const toggleFavoriteValue = e => {
+    e.preventDefault();
+    setFavoriteValue(!isFavorite);
+    dispatch(toggleFavorite(productId));
+  };
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.photo}>
+        <img
+          className={styles.image}
+          alt={name}
+          src={`${process.env.PUBLIC_URL}/images/products/${name}.jpeg`}
+        />
+        {promo && <div className={styles.sale}>{promo}</div>}
+        <div className={styles.buttons}>
+          <Button variant='small'>Quick View</Button>
+          <Button variant='small'>
+            <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
+          </Button>
+        </div>
+      </div>
+
+      <div className={styles.content}>
+        <h5>{name}</h5>
+        <div className={styles.stars}>
+          {[1, 2, 3, 4, 5].map(i => (
+            <a key={i} href='#'>
+              {i <= stars ? (
+                <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+              ) : (
+                <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+              )}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.actions}>
+        <div className={styles.outlines}>
+          <Button
+            variant='outline'
+            onClick={e => toggleFavoriteValue(e)}
+            className={favoriteValue ? styles.isFavorite : undefined}
+          >
+            <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+          </Button>
+          <Button variant='outline'>
+            <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+          </Button>
+        </div>
+        <div className={styles.price}>
+          <Button noHover variant='small'>
+            $ {price}
+          </Button>
+        </div>
       </div>
     </div>
-    <div className={styles.content}>
-      <h5>{name}</h5>
-      <div className={styles.stars}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#'>
-            {i <= stars ? (
-              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-            ) : (
-              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-            )}
-          </a>
-        ))}
-      </div>
-    </div>
-    <div className={styles.line}></div>
-    <div className={styles.actions}>
-      <div className={styles.outlines}>
-        <Button variant='outline'>
-          <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
-        </Button>
-        <Button variant='outline'>
-          <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
-        </Button>
-      </div>
-      <div className={styles.price}>
-        <Button noHover variant='small'>
-          $ {price}
-        </Button>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 ProductBox.propTypes = {
   children: PropTypes.node,
@@ -66,6 +85,8 @@ ProductBox.propTypes = {
   price: PropTypes.number,
   promo: PropTypes.string,
   stars: PropTypes.number,
+  isFavorite: PropTypes.bool,
+  id: PropTypes.string,
 };
 
 export default ProductBox;
--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,9 +5,27 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+/* actions */
+const createActionName = actionName => `app/lists/${actionName}`;
+const TOGGLE_FAVORITE = createActionName('TOGGLE_FAVORITE');
+
+/* action creators */
+
+export const toggleFavorite = payload => ({
+  type: TOGGLE_FAVORITE,
+  payload,
+});
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case TOGGLE_FAVORITE:
+      return statePart.map(products =>
+        products.id === action.payload
+          ? { ...products, isFavorite: !products.isFavorite }
+          : products
+      );
+
     default:
       return statePart;
   }
