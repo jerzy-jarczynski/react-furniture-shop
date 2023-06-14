81ed44874688d5b7d36f9a9356bcc45afec49403
Adam Żukowski <adam.zukowski471@gmail.com>
2023-06-14 10:54:41 +0200

    fix branch containt

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -9,7 +9,6 @@ import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 const Homepage = () => (
   <div className={styles.root}>
     <FeatureBoxes />
-    {/*Place for promotions */}
     <NewFurniture />
   </div>
 );
