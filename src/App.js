f05264ca23cc2e98e5f8493b033d1b8760c1eb27
Adam Żukowski <adam.zukowski471@gmail.com>
2023-06-19 23:02:54 +0200

    Add component, base version

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import Homepage from './components/views/Homepage/Homepage';
 import ProductList from './components/views/ProductList/ProductList';
 import ProductPage from './components/views/ProductPage/ProductPage';
+import SearchPage from './components/features/SearchPage/SearchPage';
 
 const App = () => (
   <Provider store={store}>
@@ -19,6 +20,7 @@ const App = () => (
           <Route exact path={'/'} component={Homepage} />
           <Route exact path={'/shop/:categoryId'} component={ProductList} />
           <Route exact path={'/product/:productId'} component={ProductPage} />
+          <Route exact path={'/search/:searchResult'} component={SearchPage} />
         </Switch>
       </MainLayout>
     </BrowserRouter>
---
+++ src/components/features/SearchPage/SearchPage.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styles from './SearchPage.module.scss';
+import FeatureBoxes from '../FeatureBoxes/FeatureBoxes';
+import Brands from '../../layout/Brands/Brands';
+import { useSelector } from 'react-redux';
+import { getAll } from '../../../redux/productsRedux';
+import { useParams } from 'react-router-dom';
+import ProductBox from '../../common/ProductBox/ProductBox';
+
+const SearchPage = () => {
+  const products = useSelector(state => getAll(state));
+  const searchResult = useParams().searchResult;
+  const filtered = [];
+  for (const product of products) {
+    if (product.name.toLowerCase().includes(searchResult)) {
+      console.log('find: ', product);
+      filtered.push(product);
+    }
+  }
+
+  console.log('prod: ', products);
+  console.log('serch: ', searchResult);
+  console.log('filter: ', filtered);
+  return (
+    <div>
+      <FeatureBoxes />
+
+      <div className={styles.root}>
+        <div className='container'>
+          <h1 className={styles.heading}>
+            Wyszukiwana fraza to: &quot;{searchResult}&quot;
+          </h1>
+          <div className='row'>
+            {filtered.map(item => (
+              <div key={item.id} className='col-12 col-sm-6 col-lg-4'>
+                <ProductBox {...item} photoClassName={styles.photo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <Brands />
+      {/* Place for Feedback section*/}
+    </div>
+  );
+};
+
+export default SearchPage;
