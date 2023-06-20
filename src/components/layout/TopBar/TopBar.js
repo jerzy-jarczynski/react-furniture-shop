5fd3d857b95f468e2e557517985207377c3bc493
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-06-20 20:29:52 +0200

    Add i18next && react-i18next, add translations setup, add translation resources, add translations in TopBar

--- src/components/layout/TopBar/TopBar.js
+++ src/components/layout/TopBar/TopBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 // import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,52 +7,61 @@ import { faCaretDown, faUser, faLock, faBars } from '@fortawesome/free-solid-svg
 
 import styles from './TopBar.module.scss';
 
-const TopBar = () => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row'>
-        <div className={`col text-left ${styles.topOptions}`}>
-          <ul>
-            <li>
-              <a href='#'>
-                USD <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-              </a>
-            </li>
-            <li>
-              <a href='#'>
-                English <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-              </a>
-            </li>
-            <li>
-              <a href='#'>
-                Help <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className={`col text-right ${styles.topMenu}`}>
-          <ul>
-            <li>
-              <a href='#'>
-                <FontAwesomeIcon className={styles.icon} icon={faUser} /> Login
-              </a>
-            </li>
-            <li>
-              <a href='#'>
-                <FontAwesomeIcon className={styles.icon} icon={faLock} /> Register
-              </a>
-            </li>
-            <li>
-              <a href='#'>
-                <FontAwesomeIcon className={styles.icon} icon={faBars} />
-              </a>
-            </li>
-          </ul>
+const TopBar = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row'>
+          <div className={`col text-left ${styles.topOptions}`}>
+            <ul>
+              <li>
+                <a href='#'>
+                  {t('topbar.currency')}
+                  <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  {t('topbar.language.english')}
+                  <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  {t('topbar.help')}
+                  <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className={`col text-right ${styles.topMenu}`}>
+            <ul>
+              <li>
+                <a href='#'>
+                  <FontAwesomeIcon className={styles.icon} icon={faUser} />{' '}
+                  {t('topbar.login')}
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <FontAwesomeIcon className={styles.icon} icon={faLock} />{' '}
+                  {t('topbar.register')}
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <FontAwesomeIcon className={styles.icon} icon={faBars} />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 // TopBar.propTypes = {};
 
