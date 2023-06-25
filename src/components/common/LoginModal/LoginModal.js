50d14af94a801813ff4dfe9d1337fb929173619d
Dominika Janisz <mikaaa366@gmail.com>
2023-06-25 22:55:24 +0200

    form

--- src/components/common/LoginModal/LoginModal.js
+++ src/components/common/LoginModal/LoginModal.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import ReactDom from 'react-dom'; //Importing ReactDom
 import styles from './LoginModal.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faMinus } from '@fortawesome/free-solid-svg-icons';
 import Button from '../../common/Button/Button';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
@@ -15,6 +15,11 @@ const LoginModal = ({ modalOpen, closeModal }) => {
     console.log('email:', formData.email);
     console.log('password:', formData.password);
   };
+  const {
+    register,
+    handleSubmit: validate,
+    formState: { errors },
+  } = useForm();
 
   const onChangeEmail = e => {
     setFormData({ ...formData, email: e.target.value });
@@ -39,11 +44,15 @@ const LoginModal = ({ modalOpen, closeModal }) => {
         <form className='col-15 col-md-10 col-lg-12'>
           <div className={styles.quitButton}>
             <Button onClick={() => closeModal(false)}>
-              <FontAwesomeIcon icon={faUser} />
+              <FontAwesomeIcon icon={faMinus} />
             </Button>
           </div>
           <h3 className='text-center'>Sign in to Bazar</h3>
           <input
+            {...register('email', {
+              required: true,
+              pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+            })}
             className='form-control my-3'
             type='text'
             aria-label='Email field'
@@ -53,6 +62,11 @@ const LoginModal = ({ modalOpen, closeModal }) => {
             onChange={onChangeEmail}
           ></input>
           <input
+            {...register('password', {
+              required: true,
+              minLength: 8,
+              pattern: /(?=.[0-9])+\D/,
+            })}
             className='form-control my-3'
             type={inputType}
             aria-label='Password field'
@@ -61,6 +75,10 @@ const LoginModal = ({ modalOpen, closeModal }) => {
             placeholder='Password*'
             onChange={onChangePassword}
           ></input>
+          {errors.email && <p className='text-danger'>Incorrect email!</p>}
+          {!errors.email && errors.password && (
+            <p className='text-danger'>Incorrect password!</p>
+          )}
           <div className='row my-3'>
             <div className=' col text-left '>
               <div className='form-check form-switch '>
@@ -91,7 +109,7 @@ const LoginModal = ({ modalOpen, closeModal }) => {
               </Link>
             </div>
             <div className='col text-right'>
-              <Button variant='main' type='submit' onClick={onClick}>
+              <Button variant='main' type='submit' onClick={validate(onClick)}>
                 Sign in
               </Button>
             </div>
