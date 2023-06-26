cd061fdc376cd5883c4cfd4d060fc48048652f60
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-06-26 21:21:12 +0200

    Change styles and validation logic

--- src/components/features/Newsletter/Newsletter.js
+++ src/components/features/Newsletter/Newsletter.js
@@ -2,60 +2,57 @@ import React from 'react';
 import styles from './Newsletter.module.scss';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Button from '../../common/Button/Button';
+//import Button from '../../common/Button/Button';
 import { faEnvelope, faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import { useForm } from 'react-hook-form';
+//import { useForm } from 'react-hook-form';
 
 const Newsletter = () => {
   const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
 
-  const handleSubmit = () => {
+  const handleSubmit = event => {
     //console.log('Email added to newsletter');
+    event.preventDefault();
+
+    if (email.trim() === '') {
+      setEmailError('The email phrase cannot be empty');
+      return;
+    }
+
+    const allowedCharacters = /^[a-zA-Z0-9\s]+$/;
+    if (!allowedCharacters.test(email)) {
+      setEmailError('The email phrase contains invalid characters');
+      return;
+    }
     setEmail('');
   };
-
-  const {
-    register,
-    handleSubmit: validate,
-    formState: { errors },
-  } = useForm();
-
-  const allowedCharacters = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
   return (
     <div className={styles.root}>
       <div className='container'>
         <p className={styles.title}>Newsletter:</p>
         <div className={styles.mailBox}>
-          <form onSubmit={validate(handleSubmit)}>
+          <form onSubmit={handleSubmit}>
             <div className={styles.formWrapper}>
               <div className='form-group '>
                 <label className={styles.iconWrapper}>
                   <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
                 </label>
                 <input
-                  {...register('email', { required: true, pattern: allowedCharacters })}
                   type='email'
                   id='email'
                   className='form-control'
                   placeholder='Enter your email address'
-                  onChange={e => setEmail(e.target.value)}
+                  onChange={event => setEmail(event.target.value)}
                   value={email}
                 />
-                {errors.email?.type === 'required' && (
-                  <small className={styles.invalidEmail}>
-                    The email phrase cannot be empty
-                  </small>
-                )}
-                {errors.email?.type === 'pattern' && (
-                  <small className={styles.invalidEmail}>
-                    The email phrase contains invalid characters
-                  </small>
+                {emailError && (
+                  <small className={styles.invalidEmail}>{emailError}</small>
                 )}
               </div>
-              <Button type='submit' variant='outline' className={styles.sendButton}>
+              <button type='submit' variant='outline' className={styles.sendButton}>
                 <FontAwesomeIcon icon={faArrowRight} />
-              </Button>
+              </button>
             </div>
           </form>
         </div>
